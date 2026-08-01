4b7be399af5f704abaf87c301ad4a55154fd0550
Arooba Shafique <224387190+arooba-shafique@users.noreply.github.com>
2026-08-01 12:34:55 +0500

    fix: add PostgreSQL support for Vercel deployment

--- movie_project/settings.py
+++ movie_project/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+import dj_database_url
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -68,10 +70,10 @@ LOGIN_URL = '/login/'
 
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default=f'sqlite:///{BASE_DIR / "db.sqlite3"}',
+        conn_max_age=600
+    )
 }
 
 AUTH_PASSWORD_VALIDATORS = [
